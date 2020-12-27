938cbd98346c9fb8e886a81f60806db534625078
Allanpassos <76652016+Allanpassos@users.noreply.github.com>
2020-12-27 10:25:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,130 @@
+const {
+    WAConnection,
+    MessageType,
+    MessageOptions
+} = require('@adiwajshing/baileys')
+const { color, bgcolor } = require('./lib/color')
+const { wait } = require('./lib/functions')
+const { getBuffer, fetchJson } = require('./lib/fetcher')
+const fs = require('fs')
+const client = new WAConnection()
+const moment = require('moment-timezone')
+const { exec } = require('child_process')
+const kagApi = require('@kagchi/kag-api')
+const fetch = require('node-fetch')
+const imgbb = require('imgbb-uploader')
+const speed = require('performance-now')
+
+async function start() {
+	client.on('qr', () => {
+		console.log(color('[','white'), color('!','red'), color(']','white'), color('Scan the qr code above'))
+	})
+
+	client.on('credentials-updated', () => {
+		fs.writeFileSync('./BarBar.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+	})
+
+	fs.existsSync('./BarBar.json') && client.loadAuthInfo('./BarBar.json')
+
+	await client.connect({timeoutMs: 30*1000})
+	
+		} catch (e) {
+			console.log('Error : %s', color(e, 'red'))
+		}
+	})
+	client.on('message-new', async (mek) => {
+		try {
+			if (!mek.message) return
+			const content = JSON.stringify(mek.message)
+			const from = mek.key.remoteJid
+			const type = Object.keys(mek.message)[0]
+			const apiKey = 'Api-Key' // You can get Apikey at https://mhankbarbars.herokuapp.com/api
+			const msgType = MessageType
+			const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
+			console.log(color('[','white'), color(time), color(']','white'), 'from', color(from.split('@')[0]), 'type' , color(type))
+			if (mek.key.fromMe) return // replace (mek.key.fromMe) to (!mek.key.fromMe) for make self bot
+			if (type == 'conversation') {
+				const body = mek.message.conversation.toLowerCase()
+				const args = body.split(' ')
+					}
+					// uncomment if you want to activate this feature
+				/*} else if (body.startsWith('!tsticker ') || body.startsWith('!tstiker ')) {
+					const teks = encodeURIComponent(body.slice(args[0].length))
+					random = `${Math.floor(Math.random() * 10000)}.png`
+					rendom = `${Math.floor(Math.random() * 20000)}.webp`
+					if (!teks) return client.sendMessage(from, 'Input teks yang ingin dijadikan stiker', msgType.text, {quoted: mek})
+					const anu = await fetchJson(`https://mhankbarbars.herokuapp.com/api/text2image?text=${teks}&apiKey=${apiKey}`, {method: 'get'})
+					exec(`wget ${anu.result} -O ${random} && ffmpeg -i ${random} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rendom}`, (error, stdout, stderr) => {
+						let buffer = fs.readFileSync(rendom)
+						client.sendMessage(from, buffer, msgType.sticker, {quoted: mek})
+						fs.unlinkSync(random)
+						fs.unlinkSync(rendom)
+					})*/
+
+			} else if (type == 'imageMessage') {
+				const captimg = mek.message.imageMessage.caption.toLowerCase()
+				if (captimg == '!stiker' || captimg == '!sticker') {
+					media = await client.downloadAndSaveMediaMessage(mek)
+					rendom = `${Math.floor(Math.random() * 10000)}.webp`
+					exec(`ffmpeg -i ${media} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rendom}`, (error, stdout, stderr) => {
+						let buffer = fs.readFileSync(rendom)
+						client.sendMessage(from, buffer, msgType.sticker, {quoted: mek})
+						fs.unlinkSync(rendom)
+						fs.unlinkSync(media)
+						
+			} else if (type == 'videoMessage') {
+				const captvid = mek.message.videoMessage.caption.toLowerCase()
+				if (captvid == '!stiker' || captvid == '!sticker') {
+					media = await client.downloadAndSaveMediaMessage(mek)
+					rendom = `${Math.floor(Math.random() * 10000)}.webp`
+					exec(`ffmpeg -i ${media} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rendom}`, (error, stdout, stderr) => {
+						let buffer = fs.readFileSync(rendom)
+						client.sendMessage(from, buffer, msgType.sticker, {quoted: mek})
+						fs.unlinkSync(rendom)
+						fs.unlinkSync(media)
+					})
+				} else {
+					return
+				}
+
+			} else if (type == 'extendedTextMessage') {
+				mok = JSON.parse(JSON.stringify(mek).replace('quotedM','m'))
+				qtdMsg = mek.message.extendedTextMessage.text.toLowerCase()
+				if (qtdMsg == '!stiker' || qtdMsg == '!sticker' && content.includes('imageMessage') || content.includes('videoMessage')) {
+					media = await client.downloadAndSaveMediaMessage(mok.message.extendedTextMessage.contextInfo)
+					rendom = `${Math.floor(Math.random() * 10000)}.webp`
+					exec(`ffmpeg -i ${media} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rendom}`, (error, stdout, stderr) => {
+						let buffer = fs.readFileSync(rendom)
+						client.sendMessage(from, buffer, msgType.sticker, {quoted: mek})
+						fs.unlinkSync(rendom)
+						fs.unlinkSync(media)
+					})
+				} else if (qtdMsg == '!wait' && content.includes('imageMessage')) {
+					media = await client.downloadAndSaveMediaMessage(mok.message.extendedTextMessage.contextInfo)
+					let buffer = fs.readFileSync(media)
+					await wait(buffer).then(res => {
+						client.sendMessage(from, res.video, msgType.video, {caption: res.hasil, quoted: mek})
+						fs.unlinkSync(media)
+					}).catch(err => {
+						client.sendMessage(from, err, msgType.text, {quoted: mek})
+						fs.unlinkSync(media)
+					})
+				} else if (qtdMsg == '!toimg' && content.includes('stickerMessage')) {
+					media = await client.downloadAndSaveMediaMessage(mok.message.extendedTextMessage.contextInfo)
+					random = `${Math.floor(Math.random() * 10000)}.png`
+					exec(`ffmpeg -i ${media} ${random}`, (error, stdout, stderr) => {
+						let buffer = fs.readFileSync(random)
+						client.sendMessage(from, buffer, msgType.image, {quoted: mek, caption: 'dah jadi kontol'})
+					})
+				} else {
+					return
+				}
+			} else {
+				return false
+			}
+		} catch (e) {
+			console.log('Error : %s', color(e, 'red'))
+		}
+	})
+}
+start().catch((err) => console.log(`Error : ${err}`))
